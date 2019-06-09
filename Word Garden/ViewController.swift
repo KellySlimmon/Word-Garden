38763121e78ac19cd000d694d7baf02842e78939
Kelly Slimmon <slimmon@bc.edu>
2019-06-09 15:29:22 +0100

    3.2 Updating UI

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var guessedLetterField: UITextField!
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    @IBOutlet weak var guessLetterButton: UIButton!
+    @IBOutlet weak var userGuessLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func updateUIAfterGuess(){
+        guessedLetterField.resignFirstResponder()
+        guessedLetterField.text = ""
+    }
+    @IBAction func guessLetterFieldChanged(_ sender: UITextField) {
+        print("Guessed Letter Field changed!")
+    }
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        updateUIAfterGuess()
+    }
+    @IBAction func guessButtonPressed(_ sender: UIButton) {
+        updateUIAfterGuess()
+    }
+    
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
 }
 
